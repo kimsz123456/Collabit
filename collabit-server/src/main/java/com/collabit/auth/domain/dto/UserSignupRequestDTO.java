7d65dffd39dedb0ec82c2e19de34309535463c91
seulgi980 <seulgi980@naver.com>
2025-02-11 18:22:38 +0900

    Feat: 임시 토큰 발급/검증 로직 추가

--- collabit-server/src/main/java/com/collabit/auth/domain/dto/UserSignupRequestDTO.java
+++ collabit-server/src/main/java/com/collabit/auth/domain/dto/UserSignupRequestDTO.java
@@ -18,14 +18,14 @@ public class UserSignupRequestDTO {
 
 
     @NotBlank(message = "닉네임을 입력해주세요")
-    @Pattern(regexp = "^.{2,16}$",
-    message = "닉네임은 2자 이상 16자 이하로 입력해주세요")
+    @Pattern(regexp = "^.{2,10}$",
+    message = "닉네임은 2자 이상 10자 이하로 입력해주세요")
     private String nickname;
 
 
     @NotBlank(message = "비밀번호를 입력해주세요")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
-            message = "비밀번호는 영문, 숫자, 특수문자를 포함하여 8~16자여야 합니다")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
+        message = "비밀번호는 영문, 숫자, 특수문자를 포함한 8자리 이상입니다.")
     private String password;
 
 }
